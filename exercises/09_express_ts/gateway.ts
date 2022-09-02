b3b8505908a58d88d4444a63499142fc72cbb2ab
bozzelliandrea <bozzelliandrea@yahoo.it>
2022-09-02 23:12:28 +0200

    gateway implementation

--- exercises/09_express_ts/gateway.ts
+++ exercises/09_express_ts/gateway.ts
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config({path: './gateway.env'});
 
 import express, {Express} from 'express';
-import http from 'http';
+import http, {RequestOptions} from 'http';
 
 const gateway: Express = express();
-
+gateway.use(express.json());
 gateway.get('/photo', (req, res) => {
     http.get(`http://localhost:3000/photo`, proxy_res => {
         let data = ''
@@ -16,11 +16,45 @@ gateway.get('/photo', (req, res) => {
         });
 
         proxy_res.on('end', () => {
+            res.setHeader('Content-Type', proxy_res.headers["content-type"]);
             res.send(data);
         });
     }).on("error", (err) => {
         console.log("Error: ", err)
     }).end()
+})
+
+gateway.post('/photo', (req, res) => {
+
+    const data = JSON.stringify(req.body);
+
+    const option: RequestOptions = {
+        hostname: 'localhost',
+        port: '3000',
+        path: '/photo',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length,
+        }
+    }
+
+    const internal_req = http.request(option, proxy_res => {
+        let data = ''
+
+        proxy_res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        proxy_res.on('end', () => {
+            res.setHeader('Content-Type', proxy_res.headers["content-type"]);
+            res.send(data);
+        });
+    }).on("error", (err) => {
+        console.log("Error: ", err)
+    });
+    internal_req.write(data);
+    internal_req.end()
 })
 
 gateway.listen(process.env.PORT, () => {
--- exercises/09_express_ts/middleware/response.middleware.ts
+++ exercises/09_express_ts/middleware/response.middleware.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express";
 
 export default function (req: Request, res: Response) {
+    res.setHeader('Content-Type', 'application/json');
     res.send({
         payload: res.locals._response,
         token: "newToken"
